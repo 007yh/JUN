dd8e4ea554f9911c46b87d0d5d5335590ce43e7d
Zero <120361899+007yh@users.noreply.github.com>
2026-03-04 16:27:14 +0800

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.cjyh.app',
-  appName: '君',
-  webDir: 'dist',
-};
-
-export default config;
